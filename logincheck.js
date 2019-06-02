af6b349969b35e05a87f42604cfa0863af1501b9
willster191 <William.C.B.19@gmail.com>
2019-06-02 02:45:02 -0500

    Leftover code
    Some code that was skipped when I last pushed

--- logincheck.js
+++ logincheck.js
@@ -2,3 +2,4 @@ function loginCheck() {
   if (sessionStorage.getItem("currentuser") === null) {
     location = 'login.html';
   }
+}//if a user is not logged in, they are returned to the home screen
